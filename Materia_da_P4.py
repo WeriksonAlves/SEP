56d836c76c0b10ed0fceb8601ad42a2cb7822660
WeriksonAlves <92264123+WeriksonAlves@users.noreply.github.com>
2022-03-27 19:41:10 -0300

    a

--- Materia_da_P4.py
+++ Materia_da_P4.py
@@ -4,28 +4,5 @@ from math import inf
 from fractions import Fraction
 import matplotlib.pyplot as plt
 
-
-# Criar uma classe para calcular o fluxo de carga em sistemas eletricos por meio do metodo de newton-raphson com sistemas de equacoes lineares
-
-
-class Newton:
-    def __init__(self, f, x0, tol=1e-6, nmax=100):
-        self.f = f
-        self.x0 = x0
-        self.tol = tol
-        self.nmax = nmax
-
-    def iterate(self):
-        x = self.x0
-        for i in range(self.nmax):
-            fx = self.f(x)
-            if fx == 0:
-                return x
-            dfx = self.f.diff(x)
-            if dfx == 0:
-                return None
-            x = x - fx/dfx
-        return None
-
-    
-    
+class FLuxo_de_carga(self, Y, x0):
+    self.Y = Y
