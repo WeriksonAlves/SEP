2a6dec90206638a43b38425b4065a1f43fe536a3
WeriksonAlves <92264123+WeriksonAlves@users.noreply.github.com>
2022-03-28 00:10:15 -0300

    a

--- Materia_da_P4.py
+++ Materia_da_P4.py
@@ -1,8 +1,78 @@
 import numpy as np
 from sympy import * #var, Lambda, exp, log, sin, cos, tan, sqrt, ln
-from math import inf
-from fractions import Fraction
-import matplotlib.pyplot as plt
+#from math import inf
+#from fractions import Fraction
+#import matplotlib.pyplot as plt
+teta2, V2 = var('teta2 V2')
 
-class FLuxo_de_carga(self, Y, x0):
-    self.Y = Y
+class Fluxo_de_carga:
+    def __init__(self, Y, G, B, y, x, J, x0, e):
+        self.Y = Y
+        self.G = G
+        self.B = B
+        self.y = y
+        self.x = x
+        self.J = J
+        self.x0 = x0
+        self.e = e
+
+        print('\nY = ', Y)
+        print('\nG = ', G)
+        print('\nB = ', B)
+
+        self.equações()
+        self.metodo_newton()
+
+    def equações(self):
+        print('\n')
+
+        for i in range(len(self.y)):
+            print('y[',i,'] = ', self.y[i])
+        print('\n')
+
+        for i in range(len(self.x)):
+            print('x[',i,'] = ', self.x[i])
+        print('\n')
+
+        for i in range(len(self.J)):
+            for j in range(len(self.J)):
+                print('J[',i,'][',j,'] = ', self.J[i][j])
+        print('\n')
+
+    def metodo_newton(self):
+        
+    
+
+        #criar uma função para calcular o metodo de newton para um sistema de equações
+        def F(x):
+            return np.dot(J, x) - y
+
+        def Fd(x):
+            return J
+
+        def main():
+            dados = {'n____':[],
+                     'Xn___':[],
+                     'e_abs':[],
+                     'e_rel':[],
+                     'Fxn__':[],
+                     'FDxn_':[]}
+            n = 0
+            dados['n____'].append(n)
+
+            #Dados
+            print('Entrada dos parâmetros:')
+            x0 = self.x0
+
+
+
+    #def Newton()
+        
+        
+
+
+
+
+    
+    
+        
