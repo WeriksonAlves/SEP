b468481d96a0047667e40c28e9c775c292268675
WeriksonAlves <92264123+WeriksonAlves@users.noreply.github.com>
2022-03-29 01:08:09 -0300

    Ta quase

--- Materia_da_P4.py
+++ Materia_da_P4.py
@@ -1,78 +1,32 @@
 import numpy as np
 from sympy import * #var, Lambda, exp, log, sin, cos, tan, sqrt, ln
-#from math import inf
-#from fractions import Fraction
-#import matplotlib.pyplot as plt
+
 teta2, V2 = var('teta2 V2')
 
 class Fluxo_de_carga:
-    def __init__(self, Y, G, B, y, x, J, x0, e):
-        self.Y = Y
-        self.G = G
-        self.B = B
-        self.y = y
-        self.x = x
-        self.J = J
-        self.x0 = x0
-        self.e = e
-
-        print('\nY = ', Y)
-        print('\nG = ', G)
-        print('\nB = ', B)
-
-        self.equações()
-        self.metodo_newton()
-
-    def equações(self):
-        print('\n')
-
-        for i in range(len(self.y)):
-            print('y[',i,'] = ', self.y[i])
-        print('\n')
-
-        for i in range(len(self.x)):
-            print('x[',i,'] = ', self.x[i])
-        print('\n')
-
-        for i in range(len(self.J)):
-            for j in range(len(self.J)):
-                print('J[',i,'][',j,'] = ', self.J[i][j])
-        print('\n')
-
-    def metodo_newton(self):
-        
+    def __init__(self, Barras, Impedancias, Matriz_Barras, Matriz_Impedancias, Ep, Eq):
+        self.Barras = Barras
+        self.Impedancias = Impedancias
+        self.Matriz_Barras = Matriz_Barras
+        self.Matriz_Impedancias = Matriz_Impedancias
+        self.Ep = Ep
+        self.Eq = Eq
     
+    def Matriz_Y(self):
+        Y = np.zero((self.Barras, self.Barras))
+        for i in range(len(self.Barras)):
+            for j in range(len(self.Barras)):
+                if (i == j):
+                    Y[i, j] = 1/self.Impedancias[i]
+                Y[i,j] = 1 / self.Matriz_Barras[i,j]
+        Y[self.Barras][self.Barras] = -1/(0.01+0.05j)
+        y21 = -1/(0.01+0.05j)
+        y11 = -y12
+        y22 = -y12
 
-        #criar uma função para calcular o metodo de newton para um sistema de equações
-        def F(x):
-            return np.dot(J, x) - y
-
-        def Fd(x):
-            return J
-
-        def main():
-            dados = {'n____':[],
-                     'Xn___':[],
-                     'e_abs':[],
-                     'e_rel':[],
-                     'Fxn__':[],
-                     'FDxn_':[]}
-            n = 0
-            dados['n____'].append(n)
-
-            #Dados
-            print('Entrada dos parâmetros:')
-            x0 = self.x0
-
-
-
-    #def Newton()
-        
-        
-
-
-
-
-    
-    
-        
+        #Matriz de admitância
+        Y = np.empty((2), dtype=complex) #ordem da matriz
+        Y = np.array([[y11, y12], [y21, y22]])
+        G = Y.real
+        B = Y.imag
+ 
